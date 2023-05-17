4c670d595a9ba4c689462cb8392f1dedc58918e7
ShevDaniil123 <95983421+ShevDaniil123@users.noreply.github.com>
2023-05-17 16:58:40 +0300

    Add files via upload

---
+++ laba5/Map.mm
@@ -0,0 +1,282 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Защита информации&#xa;&#xa;ПОРЯДОК СОЗДАНИЯ АВТОМАТИЗИРОВАННЫХ СИСТЕМ В ЗАЩИЩЕННОМ ИСПОЛНЕНИИ&#xa;&#xa;Общие положения" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1684331448769" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.592">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/standard-1.6.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Область применения" POSITION="bottom_or_right" ID="ID_1415451391" CREATED="1684328313758" MODIFIED="1684331501174" HGAP_QUANTITY="-223.60001 pt" VSHIFT_QUANTITY="1807.80004 pt">
+<edge COLOR="#ff0000"/>
+<font SIZE="16"/>
+</node>
+<node TEXT="Нормативные ссылки" FOLDED="true" POSITION="top_or_left" ID="ID_420548173" CREATED="1684328408500" MODIFIED="1684331034793" HGAP_QUANTITY="155 pt" VSHIFT_QUANTITY="-27 pt">
+<edge COLOR="#0000ff"/>
+<font SIZE="16"/>
+<node TEXT="ГОСТ 34.003" ID="ID_376049959" CREATED="1684328448979" MODIFIED="1684328480653">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ 34.201" ID="ID_1577408533" CREATED="1684328462995" MODIFIED="1684328490723">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ 34.601" ID="ID_1809454343" CREATED="1684328464283" MODIFIED="1684328504918">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ 34.602" ID="ID_64412084" CREATED="1684328465687" MODIFIED="1684328689931">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ 34.603" ID="ID_1184843464" CREATED="1684328465863" MODIFIED="1684328701993">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ 16504" ID="ID_1548152351" CREATED="1684328466027" MODIFIED="1684328711396">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р 50922" ID="ID_1558435902" CREATED="1684328466196" MODIFIED="1684328796278">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р 53114" ID="ID_1339946296" CREATED="1684328466346" MODIFIED="1684328733538">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р 54869" ID="ID_1613765203" CREATED="1684328466498" MODIFIED="1684330958994">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р 57628" ID="ID_81303026" CREATED="1684328466676" MODIFIED="1684331526766">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК 15408-1" ID="ID_758558335" CREATED="1684328466805" MODIFIED="1684328759268">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК 15408-2" ID="ID_1710762699" CREATED="1684328466940" MODIFIED="1684328771195">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК 15408-3" ID="ID_441013422" CREATED="1684328805231" MODIFIED="1684328811835">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК 18045" ID="ID_951536037" CREATED="1684328822021" MODIFIED="1684328826764">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК 21827" ID="ID_1388611371" CREATED="1684328839055" MODIFIED="1684328842737">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК 27002" ID="ID_869290001" CREATED="1684328855471" MODIFIED="1684328859343">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК 27005" ID="ID_191184085" CREATED="1684328861694" MODIFIED="1684328871120">
+<font SIZE="16"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК ТО 19791" ID="ID_1562687394" CREATED="1684328883276" MODIFIED="1684328887907">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Термины и определения" FOLDED="true" POSITION="bottom_or_right" ID="ID_1425920797" CREATED="1684328922485" MODIFIED="1684331504785" HGAP_QUANTITY="140 pt" VSHIFT_QUANTITY="459.00001 pt">
+<edge COLOR="#ff00ff"/>
+<font SIZE="16"/>
+<node TEXT="Мероприятия по защите информации" ID="ID_487203830" CREATED="1684328956385" MODIFIED="1684329045561">
+<font SIZE="16"/>
+</node>
+<node TEXT="Обработка информации" ID="ID_1923797422" CREATED="1684328977835" MODIFIED="1684329412896">
+<font SIZE="16"/>
+</node>
+<node TEXT="Система защиты информации автоматизированной системы" ID="ID_1717546113" CREATED="1684329049340" MODIFIED="1684331025402" VSHIFT_QUANTITY="-3.6 pt">
+<font SIZE="16"/>
+</node>
+<node TEXT="Информационная система" ID="ID_1271496730" CREATED="1684329049945" MODIFIED="1684329094280">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Обозначения и сокращения" FOLDED="true" POSITION="top_or_left" ID="ID_474701136" CREATED="1684329119273" MODIFIED="1684331019250" HGAP_QUANTITY="94.4 pt" VSHIFT_QUANTITY="-78 pt">
+<edge COLOR="#7c0000"/>
+<font SIZE="16"/>
+<node TEXT="АС - автоматизированная система" ID="ID_1698974574" CREATED="1684329215120" MODIFIED="1684329274184">
+<font SIZE="16"/>
+</node>
+<node TEXT="АСЗИ - автоматизированная система в защищенном исполнении" ID="ID_148806404" CREATED="1684329217008" MODIFIED="1684329270855">
+<font SIZE="16"/>
+</node>
+<node TEXT="ЗИ - защита информации" ID="ID_453235656" CREATED="1684329217500" MODIFIED="1684329267546">
+<font SIZE="16"/>
+</node>
+<node TEXT="НИР - научно-исследовательская работа" ID="ID_71391445" CREATED="1684329217855" MODIFIED="1684329288310">
+<font SIZE="16"/>
+</node>
+<node TEXT="НСД - несанкционированный доступ" ID="ID_1607070110" CREATED="1684329218047" MODIFIED="1684329366614">
+<font SIZE="16"/>
+</node>
+<node TEXT="ОКР - опытно-конструкторская работа" ID="ID_881257161" CREATED="1684329218270" MODIFIED="1684330973845">
+<font SIZE="16"/>
+</node>
+<node TEXT="ПС - программное средство" ID="ID_1837291452" CREATED="1684329218479" MODIFIED="1684329371281">
+<font SIZE="16"/>
+</node>
+<node TEXT="СЗИ - средство защиты информации" ID="ID_384491683" CREATED="1684329218736" MODIFIED="1684329373054">
+<font SIZE="16"/>
+</node>
+<node TEXT="ТЗ - техническое задание" ID="ID_1754808564" CREATED="1684329326121" MODIFIED="1684329375502">
+<font SIZE="16"/>
+</node>
+<node TEXT="ТТЗ - тактико-техническое задание" ID="ID_1733188646" CREATED="1684329326735" MODIFIED="1684329377340">
+<font SIZE="16"/>
+</node>
+<node TEXT="ТС - техническое средство" ID="ID_87409860" CREATED="1684329342501" MODIFIED="1684329379586">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Общие положения" FOLDED="true" POSITION="bottom_or_right" ID="ID_1239759584" CREATED="1684329390879" MODIFIED="1684331508200" HGAP_QUANTITY="-209.8 pt" VSHIFT_QUANTITY="-2000.40005 pt">
+<edge COLOR="#00007c"/>
+<font SIZE="16"/>
+<node TEXT="Для обработки информации, необходимость защиты которой определяется законодательством Российской Федерации или решением ее обладателя, должны создаваться АСЗИ, в которых реализованы в соответствии с действующими нормативными правовыми актами требования о ЗИ. Реализация требований о ЗИ в АСЗИ осуществляется системой ЗИ, являющейся неотъемлемой составной частью АСЗИ." ID="ID_976482252" CREATED="1684329494313" MODIFIED="1684331184803" HGAP_QUANTITY="3.8 pt" VSHIFT_QUANTITY="-10.8 pt">
+<font SIZE="14"/>
+</node>
+<node TEXT="Целью создания системы ЗИ АСЗИ является обеспечение ЗИ от неправомерного доступа, уничтожения, модифицирования, блокирования, копирования, предоставления, распространения, а также от иных неправомерных действий в отношении такой информации, соблюдение конфиденциальности информации ограниченного доступа, реализация права на доступ к информации." ID="ID_524377814" CREATED="1684329495571" MODIFIED="1684331188502" HGAP_QUANTITY="13.4 pt" VSHIFT_QUANTITY="-4.8 pt">
+<font SIZE="14"/>
+</node>
+<node TEXT="При создании (модернизации) АСЗИ необходимо руководствоваться следующими общими требованиями" FOLDED="true" ID="ID_1173641702" CREATED="1684329496253" MODIFIED="1684331179275" HGAP_QUANTITY="14.6 pt" VSHIFT_QUANTITY="1.2 pt">
+<font SIZE="14"/>
+<node TEXT="система ЗИ АСЗИ должна обеспечивать комплексное решение задач по ЗИ от НСД, от утечки защищаемой информации по техническим каналам, от несанкционированных и непреднамеренных воздействий на информацию (на носители информации) применительно к конкретной АСЗИ. Состав решаемых задач по ЗИ определяется задачами обработки информации, решаемыми с использованием АСЗИ, ее программным и аппаратным составом, конфигурацией этой системы, условиями функционирования, требованиями, предъявляемыми к обрабатываемой информации, угрозами безопасности информации;" ID="ID_607357978" CREATED="1684329554824" MODIFIED="1684329582813">
+<font SIZE="12"/>
+</node>
+<node TEXT="система ЗИ АСЗИ должна разрабатываться (проектироваться) с учетом возможности реализации требований о защите обрабатываемой информации при использовании в АСЗИ методов и программно-аппаратных средств организации сетевого взаимодействия;" ID="ID_1891542237" CREATED="1684329556343" MODIFIED="1684329593682">
+<font SIZE="12"/>
+</node>
+<node TEXT="система ЗИ АСЗИ должна создаваться с учетом обеспечения возможности формирования различных вариантов ее построения, а также расширения возможностей ее составных частей (сегментов) в зависимости от условий функционирования АСЗИ и требований о ЗИ;" ID="ID_1531228376" CREATED="1684329556749" MODIFIED="1684329776376" VSHIFT_QUANTITY="-2.4 pt">
+<font SIZE="12"/>
+</node>
+<node TEXT="ЗИ должна обеспечиваться во всех составных частях (сегментах) АСЗИ, используемых в обработке защищаемой информации;" ID="ID_28782756" CREATED="1684329557064" MODIFIED="1684329612592">
+<font SIZE="12"/>
+</node>
+<node TEXT="входящие в состав АСЗИ средства ЗИ и контроля эффективности ЗИ не должны препятствовать нормальному функционированию АСЗИ;" ID="ID_384175736" CREATED="1684329557428" MODIFIED="1684329620675">
+<font SIZE="12"/>
+</node>
+<node TEXT="программное обеспечение системы ЗИ должно быть совместимым с программным обеспечением других составных частей (сегментов) АСЗИ и не должно снижать требуемый уровень защищенности информации в АСЗИ;" ID="ID_320209127" CREATED="1684329557798" MODIFIED="1684329628227">
+<font SIZE="12"/>
+</node>
+<node TEXT="программно-технические средства, используемые для построения системы ЗИ, должны быть совместимы между собой (корректно работать совместно) и не должны снижать уровень защищенности информации в АСЗИ." ID="ID_1809668746" CREATED="1684329558097" MODIFIED="1684329636039">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Обеспечение ЗИ в АСЗИ достигается заданием, реализацией и контролем выполнения требований о защите информации" FOLDED="true" ID="ID_703827644" CREATED="1684329496703" MODIFIED="1684331176471" HGAP_QUANTITY="17 pt" VSHIFT_QUANTITY="12 pt">
+<font SIZE="14"/>
+<node TEXT="К процессу хранения, передачи и обработки защищаемой в АСЗИ информации;" ID="ID_716592853" CREATED="1684329751236" MODIFIED="1684329770466">
+<font SIZE="12"/>
+</node>
+<node TEXT="К системе ЗИ;" ID="ID_226202146" CREATED="1684329782378" MODIFIED="1684329801538">
+<font SIZE="12"/>
+</node>
+<node TEXT="К взаимодействию АСЗИ с другими АС;" ID="ID_1146646299" CREATED="1684329782885" MODIFIED="1684329819357">
+<font SIZE="12"/>
+</node>
+<node TEXT="К условиям функционирования АСЗИ;" ID="ID_276434300" CREATED="1684329783023" MODIFIED="1684329831752">
+<font SIZE="12"/>
+</node>
+<node TEXT="К содержанию работ по созданию (модернизации) АСЗИ на различных стадиях и этапах ее создания (модернизации);" ID="ID_655897032" CREATED="1684329783151" MODIFIED="1684329842556">
+<font SIZE="12"/>
+</node>
+<node TEXT="К организациям (должностным лицам), участвующим в создании (модернизации) и эксплуатации АСЗИ;" ID="ID_882494119" CREATED="1684329783278" MODIFIED="1684329869905">
+<font SIZE="12"/>
+</node>
+<node TEXT="К документации на АСЗИ;" ID="ID_798002577" CREATED="1684329872008" MODIFIED="1684329891681">
+<font SIZE="12"/>
+</node>
+<node TEXT="К АСЗИ в целом;" ID="ID_1953648868" CREATED="1684329872420" MODIFIED="1684329906004">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="АСЗИ должны создаваться в соответствии с ТЗ, являющимся основным документом, на основании которого выполняются работы, необходимые для создания (модернизации) АСЗИ в целом и ее системы ЗИ, и осуществляется приемка этих работ заказчиком." ID="ID_1477006913" CREATED="1684329497090" MODIFIED="1684329926388">
+<font SIZE="14"/>
+</node>
+<node TEXT="Процесс создания АСЗИ должен представлять собой совокупность упорядоченных во времени, взаимосвязанных, объединенных в стадии и этапы работ, выполнение которых необходимо и достаточно для создания АСЗИ, соответствующей заданным к ней требованиям." ID="ID_1395176863" CREATED="1684329497477" MODIFIED="1684329936623">
+<font SIZE="14"/>
+</node>
+<node TEXT="В работах по созданию (модернизации) АСЗИ и ее системы ЗИ участвуют" FOLDED="true" ID="ID_1656792816" CREATED="1684329497818" MODIFIED="1684330724906">
+<font SIZE="14"/>
+<node TEXT="Заказчик АСЗИ - в части задания в ТЗ на АСЗИ и систему ЗИ, включения в документацию на АСЗИ обоснованных требований о защите обрабатываемой АСЗИ информации и контроля их выполнения в процессе экспертизы документации, испытаний и приемки как АСЗИ в целом, так и системы ЗИ, а также в части организации аттестации АСЗИ на соответствие требованиям безопасности информации и сопровождения АСЗИ в ходе ее эксплуатации;" ID="ID_164888821" CREATED="1684330727015" MODIFIED="1684330750911">
+<font SIZE="12"/>
+</node>
+<node TEXT="Разработчик АСЗИ - в части обеспечения соответствия разрабатываемой АСЗИ и ее системы ЗИ требованиям ТЗ, нормативных правовых актов, методических документов и национальных стандартов в области защиты информации" ID="ID_460039494" CREATED="1684330727647" MODIFIED="1684330759056">
+<font SIZE="12"/>
+</node>
+<node TEXT="Изготовитель ТС и ПС - в части осуществления технических мер по обеспечению соответствия изготавливаемых ТС и ПС заданным требованиям о защите обрабатываемой АСЗИ информации, в соответствии с документацией на АСЗИ;" ID="ID_1342261736" CREATED="1684330727806" MODIFIED="1684330769479">
+<font SIZE="12"/>
+</node>
+<node TEXT="Поставщик ТС и ПС - в части поставки ТС и ПС для АСЗИ, соответствующих требованиям по ЗИ, по заказу изготовителя, разработчика или заказчика и их гарантийного и послегарантийного обслуживания." ID="ID_383525931" CREATED="1684330727978" MODIFIED="1684330815876">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Организации, участвующие в создании АСЗИ, в случае выполнения работ и оказания услуг в области ЗИ, деятельность по осуществлению которых подлежит лицензированию, должны иметь соответствующие лицензии [1-4]." ID="ID_1898720970" CREATED="1684329498169" MODIFIED="1684330833162">
+<font SIZE="14"/>
+</node>
+<node TEXT="Стадии и этапы работ по созданию АСЗИ устанавливаются в ТЗ на АСЗИ на основе ГОСТ 34.601 с учетом положений настоящего стандарта." ID="ID_804631452" CREATED="1684329498527" MODIFIED="1684330844004">
+<font SIZE="14"/>
+</node>
+<node TEXT="Типовое содержание работ в части создания системы ЗИ (на определенных в ГОСТ 34.601 стадиях и этапах создания АСЗИ) приведено в разделе 6. Типовое содержание и порядок выполнения работ по ЗИ о создаваемой АСЗИ приведено в разделе 7. Состав и содержание работ могут уточняться в соответствии с требованиями нормативных правовых актов и методических документов уполномоченных федеральных органов исполнительной власти и национальных стандартов по ЗИ, а также в соответствии с требованиями заказчика АСЗИ." ID="ID_1271633585" CREATED="1684329498859" MODIFIED="1684330854546">
+<font SIZE="14"/>
+</node>
+<node TEXT="В ТЗ на создание АСЗИ для реализуемых в соответствии с ним стадий и этапов ее создания должны включаться требования к системе ЗИ АСЗИ, а также требования по ЗИ о создаваемой АСЗИ в соответствии с настоящим стандартом, нормативными правовыми актами и методическими документами уполномоченных федеральных органов исполнительной власти и другими национальными стандартами." ID="ID_668896605" CREATED="1684329499274" MODIFIED="1684330881651">
+<font SIZE="14"/>
+</node>
+</node>
+</node>
+</map>
